c8bbdc939a2a64eae4d0f869fc36bcc885842987
Preetam Jinka <pj@preet.am>
2017-11-08 08:12:48 -0500

    update phrasing

--- js/Analysis.js
+++ js/Analysis.js
@@ -84,29 +84,29 @@ var Comments = function(tables) {
     vnode.state.tables.forEach(function(t) {
       switch (t.accessType) {
       case "ALL":
-        t.comment = "There is a full scan on this table.";
+        t.comment = "The entire table is scanned.";
         break;
       case "index":
-        t.comment = "There is a full index scan on this table.";
+        t.comment = "An entire index is scanned.";
         break;
       case "range":
-        t.comment = "This table is accessed using a range read.";
+        t.comment = "A range of rows are accessed using an index.";
         break;
       case "ref":
-        t.comment = "Rows are being accessed from this table using an index.";
+        t.comment = "Matching rows are being accessed.";
         break;
       case "eq_ref":
-        t.comment = "At most one row is being accessed from this table using an index.";
+        t.comment = "At most one row is accessed from this table using an index.";
         break;
       case "const":
-        t.comment = "This table is being read once at the beginning of the query and is effectively a constant.";
+        t.comment = "This table is read once at the beginning of the query and is effectively a constant.";
       }
 
       if (t.key) {
         if (t.key == "PRIMARY") {
-          t.comment += " The primary key is being used.";
+          t.comment += " MySQL is using the PRIMARY KEY.";
         } else {
-          t.comment += " The index '" + t.key + "' is being used.";
+          t.comment += " MySQL is using the '" + t.key + "' index.";
         }
       }
     })
